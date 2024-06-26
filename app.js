b1660c40febba2bb97a468c3764c10d4d7c936b0
simbu karan <simbukaran390@gmail.com>
2024-06-26 13:02:25 +0530

    static folder update'

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 const url = process.env.MONGO_URI;
 mongoose.connect(url);
